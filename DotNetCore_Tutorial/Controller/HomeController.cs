317efe2c6488f2483a9fd3e217b06af0b9dd16aa
Amanpreet <Amanpreet.singh@mymindinfotech.com>
2019-10-29 18:36:33 +0530

    Add project files.

---
+++ DotNetCore_Tutorial/Controller/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCore_Tutorial.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore_Tutorial
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        public IActionResult Index()
+        {
+            return View(_employeeRepository.GetEmployees());
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(_employeeRepository.GetEmployee(id));
+        }
+
+    }
+}
---
+++ DotNetCore_Tutorial/Models/MocEmployeeRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCore_Tutorial.Models
+{
+    public class MocEmployeeRepository : IEmployeeRepository
+    {
+        private IList<Employee> _employees = new List<Employee>();
+        public MocEmployeeRepository()
+        {
+            _employees.Add(new Employee { Id = 1, Name = "Amanpreet", Designation = "Director" });
+            _employees.Add(new Employee { Id = 2, Name = "Rahul", Designation = "Engineer" });
+            _employees.Add(new Employee { Id = 3, Name = "Shyam", Designation = "Trainee" });
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return _employees.FirstOrDefault(e => e.Id == id);
+        }
+
+        public IList<Employee> GetEmployees()
+        {
+            return _employees;
+        }
+    }
+}
